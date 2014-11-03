04ca5b0d628bd91567e0bbcda3e6af009926b968
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-03 14:48:31 -0700

    Add basic types

--- src/fingertree/lib.rs
+++ src/fingertree/lib.rs
@@ -1,3 +1,40 @@
-#[test]
-fn it_works() {
+#![crate_name="fingertree"]
+#![crate_type="lib"]
+
+#![license = "MIT"]
+#![doc(html_root_url = "http://darinmorrison.github.io/fingertree.rs/doc/fingertree/")]
+
+//! This crate implements the Finger Tree data type.
+
+#![allow(dead_code)]
+#![feature(struct_variant)]
+
+#[deriving(Clone)]
+#[deriving(Eq)]
+#[deriving(Ord)]
+#[deriving(PartialEq)]
+#[deriving(PartialOrd)]
+#[deriving(Show)]
+enum Digit<A> {
+    One(A),
+    Two(A,A),
+    Three(A,A,A),
+    Four(A,A,A,A),
+}
+
+#[deriving(Clone)]
+#[deriving(Eq)]
+#[deriving(Ord)]
+#[deriving(PartialEq)]
+#[deriving(PartialOrd)]
+#[deriving(Show)]
+enum FingerTree<V,A> {
+    Empty,
+    Single(A),
+    Deep {
+        measure: V,
+        prefix:  Digit<A>,
+        tree:    Box<FingerTree<V,A>>,
+        suffix:  Digit<A>,
+    },
 }
