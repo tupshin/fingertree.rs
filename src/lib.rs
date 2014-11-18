3272283c5193449d55536b81df115edd2654a4d0
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-18 15:25:14 -0700

    Remove struct_variant feature (defaulted now)

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@
 #![license = "MIT"]
 #![doc(html_root_url = "http://www.rust-ci.org/epsilonz/fingertree.rs/doc/fingertree/")]
 
-#![feature(struct_variant)]
-
 //! This crate implements the Finger Tree data type.
 
 extern crate algebra;
